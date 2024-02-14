5cc953d7d0eb5179c09fb435891ad4a6c4768316
Dilmuhammad <145844965+Dilmuhammad06@users.noreply.github.com>
2024-02-15 04:29:14 +0500

    Delete myweb directory

--- myweb/student/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import IMages
-
-admin.site.register([IMages])
--- myweb/student/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import IMages
-
-class ImageForms(forms.ModelForm):
-    class Meta:
-        model = IMages
-        fields = ['name','image']
